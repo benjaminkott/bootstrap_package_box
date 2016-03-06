3c77d3ea7c1c80338273ea2d061970cd537ccc7e
Benjamin Kott <benjamin.kott@wfp2.com>
2016-03-06 12:26:25 +0100

    [TASK] Update apache and php

--- puppet/manifests/web.pp
+++ puppet/manifests/web.pp
@@ -3,19 +3,17 @@ stage { 'pre': before => Stage['main'] }
 class { 'bk2k': stage => 'pre'  }
 include bk2k
 
-$apache_user  = "vagrant"
-$apache_group = "vagrant"
-
 ## Install MySQL Client, Dumps the database and install MySQL Server
 bk2k::mysql::client { "mysql":
 
 }->
+
 bk2k::mysql::server { "mysql_server":
   database => "bootstrap",
 }->
 
 ## Install PHP
-bk2k::php { "php5":
+bk2k::php { "php7":
 
 }->
 
@@ -23,9 +21,5 @@ bk2k::php { "php5":
 bk2k::apache { "bootstrap":
   hostname                 => "bootstrap",
   port                     => 80,
-  directory_allow_override => "All",
-  run_user                 => "${apache_user}",
-  run_group                => "${apache_group}",
-  create_log               => true,
-  create_phpmyadmin        => true
+  create_log               => true
 }
--- puppet/modules/bk2k/manifests/apache.pp
+++ puppet/modules/bk2k/manifests/apache.pp
@@ -1,17 +1,29 @@
 define bk2k::apache(
     $hostname                 = undef,
     $port                     = 80,
-    $directory_allow_override = "None",
-    $run_user                 = "vagrant",
-    $run_group                = "vagrant",
-    $create_log               = false,
-    $create_phpmyadmin        = false,
-    $serveraliases            = ''
+    $create_log               = false
 ) {
 
+    #
+    # Apache
+    #
+    class { 'apache':
+        user => 'vagrant',
+        group => 'vagrant',
+        default_mods => true,     
+    } 
+    include apache::mod::rewrite
+    include apache::mod::proxy
+    include apache::mod::proxy_http
+    include apache::mod::fastcgi
+    apache::mod { 'proxy_fcgi': }
+    
+    #
+    # Install Bootstrap Package
+    #
     file { "/var/www":
         ensure => "directory"
-    }->
+    }
     file { "/var/www/${hostname}":
         ensure => link,
         target => "/vagrant/",
@@ -22,56 +34,46 @@ define bk2k::apache(
         target => "../assets/",
         force  => true;
     }
-    class { 'apache':
-
-    }
     exec { "ApacheUserChange" :
-        command => "sed -i 's/APACHE_RUN_USER=www-data/APACHE_RUN_USER=${run_user}/' /etc/apache2/envvars",
+        command => "sed -i 's/APACHE_RUN_USER=www-data/APACHE_RUN_USER=vagrant/' /etc/apache2/envvars",
         onlyif  => "grep -c 'APACHE_RUN_USER=www-data' /etc/apache2/envvars",
         path    => ["/bin/"],
-        require => Package['apache'],
+        require => Package['apache2'],
         notify  => Service["apache2"],
     }
     exec { "ApacheGroupChange" :
-        command => "sed -i 's/APACHE_RUN_GROUP=www-data/APACHE_RUN_GROUP=${run_group}/' /etc/apache2/envvars",
+        command => "sed -i 's/APACHE_RUN_GROUP=www-data/APACHE_RUN_GROUP=vagrant/' /etc/apache2/envvars",
         onlyif  => "grep -c 'APACHE_RUN_GROUP=www-data' /etc/apache2/envvars",
         path    => ["/bin/"],
-        require => Package['apache'],
+        require => Package['apache2'],
         notify  => Service["apache2"],
     }
     exec { "apache_logfile_permissions" :
         command => "chmod -R a+rX /var/log/apache2",
         path    => ["/bin/"],
-        require => Package['apache'],
+        require => Package['apache2'],
     }
     exec { "apache_logrotate_permissions" :
         command => "sed -i 's/640/644/' /etc/logrotate.d/apache2",
         path    => ["/bin/"],
-        require => Package['apache'],
+        require => Package['apache2'],
     }
-    exec { "apache_lockfile_permissions" :
-        command => "chown -R ${run_user}:${run_group} /var/lock/apache2",
+    exec { "fix_fastcgi_permissions" :
+        command => "chown -R vagrant:vagrant /var/lib/apache2/fastcgi",
         path    => ["/bin/"],
-        require => Package['apache'],
-    }
-    exec { "restart_apache2":
-        command => "/etc/init.d/apache2 restart"
+        require => Package['apache2'],
     }
     apache::vhost { "${hostname}.dev.local.${port}":
-        docroot                  => "/var/www/${hostname}/web",
-        server_name              => "${hostname}.dev.local",
-        template                 => "bk2k/apache/vhost.conf.erb",
-        priority                 => 25,
-        port                     => $port,
-        docroot_owner            => $run_user,
-        docroot_group            => $run_group,
-        directory_allow_override => "${directory_allow_override}",
-        serveraliases            => $serveraliases
+        docroot             => "/var/www/${hostname}/web",
+        servername          => "${hostname}.dev.local",
+        priority            => false,
+        port                => $port,
+        docroot_owner       => "vagrant",
+        docroot_group       => "vagrant",
+        override            => 'All',        
+        custom_fragment     => "ProxyPassMatch ^/(.*\\.php(/.*)?)$ fcgi://127.0.0.1:9000/var/www/${hostname}/web/$1",
     }
-    apache::module { "rewrite":
 
-    }
-  
     #
     # Install PIMPMYLOG
     #
@@ -81,24 +83,20 @@ define bk2k::apache(
             ensure => "directory"
         }->
         apache::vhost { "log.dev.local.80":
-            docroot                  => "/var/www/log",
-            server_name              => "log.dev.local",
-            template                 => "bk2k/apache/vhost.conf.erb",
-            priority                 => 50,
-            port                     => $port,
-            docroot_owner            => $run_user,
-            docroot_group            => $run_group,
-            directory_allow_override => "All"
+            docroot             => "/var/www/log",
+            servername          => "log.dev.local",
+            priority            => false,
+            port                => $port,
+            docroot_owner       => "vagrant",
+            docroot_group       => "vagrant",
+            override            => 'All',
+            custom_fragment     => "ProxyPassMatch ^/(.*\\.php(/.*)?)$ fcgi://127.0.0.1:9000/var/www/log/$1",
         }->
         exec { "composer_require_pimp_my_log":
             environment => ["COMPOSER_HOME=/home/vagrant/.composer"],
             command     => "composer require \"potsky/pimp-my-log\"",
             path        => ["/usr/local/bin/", "/usr/bin/", "/bin/"],
             cwd         => "/var/www/log"
-        }->
-        exec { "create_rights_for_log_folder":
-            command => "chmod 777 /var/www/log",
-            path    => ["/bin"]
         }->
         file { "/var/www/log/index.php":
             ensure => link,
@@ -112,37 +110,6 @@ define bk2k::apache(
         file { "/var/www/log/.htaccess":
             ensure  => present,
             content => template("bk2k/pimpmylog/pimpmylog.htaccess.erb")
-        }
-    }
-  
-    #
-    # Install PHPMYADMIN
-    #
-    if $create_phpmyadmin
-    {
-        file { "/var/www/phpmyadmin":
-            ensure => "directory"
-        }->
-        apache::vhost { "phpmyadmin.dev.local.80":
-            docroot                  => "/var/www/phpmyadmin/vendor/phpmyadmin/phpmyadmin",
-            server_name              => "phpmyadmin.dev.local",
-            template                 => "bk2k/apache/vhost.conf.erb",
-            priority                 => 50,
-            port                     => $port,
-            docroot_owner            => $run_user,
-            docroot_group            => $run_group,
-            directory_allow_override => "All"
-        }->
-        exec { "composer_require_phpmyadmin":
-            environment => ["COMPOSER_HOME=/home/vagrant/.composer"],
-            command     => "composer require \"phpmyadmin/phpmyadmin\"",
-            path        => ["/usr/local/bin/", "/usr/bin/", "/bin/"],
-            cwd         => "/var/www/phpmyadmin",
-            require     => Exec['set_path_for_composer_bin']
-        }->
-        file { "/var/www/phpmyadmin/vendor/phpmyadmin/phpmyadmin/config.inc.php":
-            ensure  => present,
-            content => template("bk2k/phpmyadmin/phpmyadmin.config.erb")
         }
     }
 
